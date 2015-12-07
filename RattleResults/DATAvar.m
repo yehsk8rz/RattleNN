1e6b1b56700b8f08c13070141088874714603ecf
yehsk8rz <lab@eoclab2.ucmerced.edu>
2015-12-07 11:40:19 -0800

    ifourier 12-2 with xshift=0 data.csv files added ready for analysis

--- RattleResults/DATAvar.m
+++ RattleResults/DATAvar.m
@@ -1,15 +1,15 @@
-datafile = fopen('data.csv','w')
+datafile = fopen('800data.csv','w')
 %run 1-10
 for t = 1:5
     load(['rattle_daspnet_reservoir_run',num2str(t),'.mat']);
-    for t = 1:1000
+    for t = 1:800
         fprintf(datafile,[id,',',num2str(0),',',num2str(t),',',num2str(trialInfo(1,t)),',',num2str(trialInfo(2,t)),'\n']);
 end
 end
 %yoked runs 1-10
 for t = 1:5
     load(['rattle_daspnet_reservoir_run',num2str(t),'_yoke.mat']);
-    for t = 1:1000
+    for t = 1:800
         fprintf(datafile,[id,',',num2str(1),',',num2str(t),',',num2str(trialInfo(1,t)),',',num2str(trialInfo(2,t)),'\n']);
     end
 end
