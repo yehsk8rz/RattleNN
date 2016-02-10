57b90c2867eba910e1aa41391dcb99391a7f04f9
yehsk8rz <lab@eoclab2.ucmerced.edu>
2016-02-10 14:22:28 -0800

    ran fsine for RattleC results ready for analysis

--- RattleResults/DATAvar.m
+++ RattleResults/DATAvar.m
@@ -1,15 +1,15 @@
-datafile = fopen('1200data.csv','w')
+datafile = fopen('300data.csv','w')
 %run 1-10
 for t = 1:5
     load(['rattle_daspnet_reservoir_run',num2str(t),'.mat']);
-    for t = 1:1200
+    for t = 1:300
         fprintf(datafile,[id,',',num2str(0),',',num2str(t),',',num2str(trialInfo(1,t)),',',num2str(trialInfo(2,t)),'\n']);
 end
 end
 %yoked runs 1-10
 for t = 1:5
     load(['rattle_daspnet_reservoir_run',num2str(t),'_yoke.mat']);
-    for t = 1:1200
+    for t = 1:300
         fprintf(datafile,[id,',',num2str(1),',',num2str(t),',',num2str(trialInfo(1,t)),',',num2str(trialInfo(2,t)),'\n']);
     end
 end
